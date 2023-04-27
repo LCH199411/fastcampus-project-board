9116b6497a573c21a268c605dfa8f81ac6dc0ea0
jma199455 <jma199455@gmail.com>
2023-04-27 17:49:22 +0900

    #10 - Spring Data JPA 속성과 설정 * jpa auditing 기능을 쓰기 위해 `JpaConfig.java` 추가 * db 접근과 개발 편의를 위한 각종 부트 프로퍼티 적용 * TODO : 패스워드를 프로퍼티에 노출시켰다. 처리필요

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -5,18 +5,23 @@ import com.fastcampus.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+@ActiveProfiles("testdb")
+// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 테스트 상태에서 돌린다 해도 테스트 디비를 돌리지 않고 설정된 걸 쓴다.
+                                                                                // application.yaml 파일에 test.database.replace: none 설정했기 때문에 쓰지 않아도 됨
 @DisplayName("JAP 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
-class JpaRepositoryTest {
+class JpaRepositoryTest { //  수정확인
 
     private ArticleRepository articleRepository;
     private ArticleCommentRepository articleCommentRepository;
