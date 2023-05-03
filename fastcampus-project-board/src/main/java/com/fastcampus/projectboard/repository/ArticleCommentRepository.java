73212081d600a81471eb9b48a7c00640d34edc43
jma199455 <jma199455@gmail.com>
2023-05-03 17:16:59 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가 `QuerydslPredicateExecutor` `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -1,11 +1,29 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.ArticleComment;
+import com.fastcampus.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>, // 기본적으로 엔티티안에 있는 모든 필드에 대한 모든 검색 기능 추가를 해준다.
+        QuerydslBinderCustomizer<QArticleComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) { // 자바 8 이후 인터페이스에서 구현이 가능해짐
+        bindings.excludeUnlistedProperties(true); // 선택적으로 검색 가능하게 하기 위해 사용
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // date 타입이므로
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 
 
 }
